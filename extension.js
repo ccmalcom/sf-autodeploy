5b6d6f029f3cd140e7c903a135928a455311ba6a
ccmalcom <chasecmalcom@gmail.com>
2024-10-02 12:43:14 -0400

    updates

--- extension.js
+++ extension.js
@@ -3,16 +3,14 @@ const { exec } = require('child_process');
 class StatusBarItem {
 	constructor() {
 		this.statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-		this.statusBarItem.text = 'AutoDeploy: Not Watching';
-		this.statusBarItem.command = 'sf-autodeploy.startWatching';
-		this.statusBarItem.tooltip = 'Click to select files to watch';
+		this.reset();  // Default state is "Click to Select Files"
 		this.statusBarItem.show();
 	}
 
 	watching(numItems) {
 		this.statusBarItem.text = `AutoDeploy: Watching ${numItems} item(s)`;
 		this.statusBarItem.command = 'sf-autodeploy.stopWatching';
-		this.statusBarItem.tooltip = 'Click to stop watching files';
+		this.statusBarItem.tooltip = 'Click to stop watching files or select new items';
 	}
 
 	deployed() {
@@ -20,15 +18,20 @@ class StatusBarItem {
 		this.statusBarItem.command = 'sf-autodeploy.stopWatching';
 	}
 
-
 	deployError() {
 		this.statusBarItem.text = 'AutoDeploy: Error Deploying';
 		this.statusBarItem.command = 'sf-autodeploy.stopWatching';
 	}
 
-	stopped() {
+	noWatch() {
 		this.statusBarItem.text = 'AutoDeploy: Not Watching';
-		this.statusBarItem.command = 'sf-autodeploy.startWatching';
+		this.statusBarItem.command = 'sf-autodeploy.startWatching';  // Start watching when clicked
+		this.statusBarItem.tooltip = 'Click to start watching configured files';
+	}
+
+	reset() {
+		this.statusBarItem.text = 'AutoDeploy: No Files Selected';
+		this.statusBarItem.command = 'sf-autodeploy.selectItems';  // Go back to select files
 		this.statusBarItem.tooltip = 'Click to select files to watch';
 	}
 
@@ -37,7 +40,9 @@ class StatusBarItem {
 	}
 }
 
-let watchers, statusBarItem;
+let watchers = [];
+let statusBarItem;
+let selectedFilePaths = [];
 
 function checkSFDXCLI() {
 	exec('sfdx --version', (error) => {
@@ -52,16 +57,15 @@ function checkSFDXCLI() {
  */
 function activate(context) {
 	checkSFDXCLI();
+	statusBarItem = new StatusBarItem();
+	context.subscriptions.push(statusBarItem);
 	const defaultUri = vscode.workspace.workspaceFolders
 		? vscode.workspace.workspaceFolders[0].uri.with({ path: vscode.workspace.workspaceFolders[0].uri.path + '/force-app/main/default' })
 		: vscode.Uri.file(require('os').homedir());
 
-	statusBarItem = new StatusBarItem();
-	context.subscriptions.push(statusBarItem);
 
 	watchers = [];
-
-	const cmdStartWatching = vscode.commands.registerCommand('sf-autodeploy.startWatching', function () {
+	const cmdSelectItems = vscode.commands.registerCommand('sf-autodeploy.selectItems', function () {
 		vscode.window.showOpenDialog({
 			canSelectFiles: true,
 			canSelectFolders: true,
@@ -69,11 +73,7 @@ function activate(context) {
 			defaultUri: defaultUri,
 			openLabel: 'Select Files or Folders to Watch',
 			filters: {
-				'All Salesforce Files': ['js', 'html', 'css', 'cls', 'cmp', 'xml', 'svg', 'json'],
-				'JavaScript Files': ['js'],
-				'HTML Files': ['html'],
-				'Apex Classes': ['cls'],
-				'Components': ['cmp', 'xml']
+				'All Salesforce Files': ['js', 'html', 'cls', 'cmp', 'xml', 'svg', 'json']
 			}
 		}).then(selectedItems => {
 			if (selectedItems) {
@@ -81,49 +81,75 @@ function activate(context) {
 				if (invalidSelections.length > 0) {
 					vscode.window.showWarningMessage('Please select only files from within the force-app/main/default directory.');
 				} else {
-					vscode.window.showInformationMessage(`Watching ${selectedItems.length} item(s) for changes...`);
-					statusBarItem.watching(selectedItems.length);
-					selectedItems.forEach(item => {
-						const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(item.fsPath, '**/*'));
-						watcher.onDidChange(uri => {
-							statusBarItem.text = 'AutoDeploy: File Changed - Deploying...';
-							statusBarItem.command = '';
-							deployFile(uri.fsPath)
-								.then(() => {
-									vscode.window.showInformationMessage(`Successfully Deployed ${uri.fsPath}`);
-									statusBarItem.deployed();
-									setTimeout(() => {
-										statusBarItem.watching(selectedItems.length);
-									}, 3000);
-								})
-								.catch(err => {
-									vscode.window.showErrorMessage(`Error deploying ${uri.fsPath}: ${err.message}`);
-									statusBarItem.deployError();
-									setTimeout(() => {
-										statusBarItem.watching(selectedItems.length);
-									}, 3000);
-								});
-						});
-						watchers.push(watcher);
-						context.subscriptions.push(watcher);
-					});
+					const filePaths = selectedItems.map(item => item.fsPath);
+					selectedFilePaths = filePaths;
+					vscode.window.showInformationMessage(`Saved ${filePaths.length} items to configuration.`);
+					statusBarItem.noWatch();
 				}
 			}
 		});
 	});
+	context.subscriptions.push(cmdSelectItems);
+
+	const cmdStartWatching = vscode.commands.registerCommand('sf-autodeploy.startWatching', function () {
+		const filePaths = selectedFilePaths;
+		if (filePaths.length > 0) {
+			watchFiles(filePaths);
+		} else {
+			vscode.window.showWarningMessage('No files selected. Invoking Select Items Command.');
+			vscode.commands.executeCommand('sf-autodeploy.selectItems');
+		}
+	});
 	context.subscriptions.push(cmdStartWatching);
 
 	const cmdStopWatching = vscode.commands.registerCommand('sf-autodeploy.stopWatching', function () {
-		if (watchers.length > 0) {
-			watchers.forEach(watcher => watcher.dispose());
-			watchers = [];
-			vscode.window.showInformationMessage('Stopped watching files.');
-			statusBarItem.stopped();
-		} else {
-			vscode.window.showInformationMessage('No active watchers to stop.');
-		}
+		stopWatchingFiles();
+		//get config for watched files
+		const filePaths = selectedFilePaths;
+		vscode.window.showInformationMessage(`Stopped watching files. Configuration unchanged. Configured files: ${filePaths}`);
+		statusBarItem.noWatch();
 	});
 	context.subscriptions.push(cmdStopWatching);
+
+	const cmdReset = vscode.commands.registerCommand('sf-autodeploy.reset', function () {
+		stopWatchingFiles();
+		selectedFilePaths = [];
+		vscode.window.showInformationMessage('Configuration reset. No files are being watched.');
+		statusBarItem.reset();
+	});
+	context.subscriptions.push(cmdReset);
+}
+
+function watchFiles(filePaths) {
+	if (filePaths.length === 0) return;
+
+	filePaths.forEach(filePath => {
+		const watcher = vscode.workspace.createFileSystemWatcher(new vscode.RelativePattern(filePath, '**/*'));
+
+		watcher.onDidChange(uri => {
+			statusBarItem.text = 'AutoDeploy: File Changed - Deploying...';
+			deployFile(uri.fsPath).then(() => {
+				statusBarItem.deployed();
+				setTimeout(() => statusBarItem.watching(filePaths.length), 3000);
+			}).catch(() => {
+				statusBarItem.deployError();
+				setTimeout(() => statusBarItem.watching(filePaths.length), 3000);
+			});
+		});
+
+		watchers.push(watcher);
+	});
+
+	statusBarItem.watching(filePaths.length);
+	vscode.window.showInformationMessage(`Watching ${filePaths.length} file(s) for changes.`);
+}
+
+function stopWatchingFiles() {
+	if (watchers.length > 0) {
+		watchers.forEach(watcher => watcher.dispose());
+		watchers = [];
+		vscode.window.showInformationMessage('Stopped watching files.');
+	}
 }
 
 /**
@@ -131,21 +157,18 @@ function activate(context) {
  * @param {string} filePath - The path of the file that was modified.
  */
 function deployFile(filePath) {
-	vscode.window.showInformationMessage(`Deploying ${filePath}...`);
-
-	const terminal = vscode.window.createTerminal(`Deploy ${filePath}`);
-	terminal.sendText(`sfdx force:source:deploy -p ${filePath}`);
-	terminal.show();
+	return new Promise((resolve) => {
+		const terminal = vscode.window.createTerminal(`Deploy ${filePath}`);
+		terminal.sendText(`sfdx force:source:deploy -p ${filePath}`);
+		terminal.show();
+		resolve();  // You can simulate deployment success or failure here
+	});
 }
 
 function deactivate() {
-	if (watchers.length > 0) {
-		watchers.forEach(watcher => watcher.dispose());
-		watchers = [];
-
-		if (statusBarItem) {
-			statusBarItem.dispose();
-		}
+	stopWatchingFiles();
+	if (statusBarItem) {
+		statusBarItem.dispose();
 	}
 }
 
